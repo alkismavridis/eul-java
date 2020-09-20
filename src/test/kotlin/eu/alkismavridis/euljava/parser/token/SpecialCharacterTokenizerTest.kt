4cf43f1c1e88f60c92d3cebcb4d9212be0cd4686
Roja <roja_2400@yahoo.de>
2020-09-20 20:09:19 +0200

    Unittests SpecitalCharacterTokenizerTest test snack snack

--- src/test/kotlin/eu/alkismavridis/euljava/parser/token/SpecialCharacterTokenizerTest.kt
+++ src/test/kotlin/eu/alkismavridis/euljava/parser/token/SpecialCharacterTokenizerTest.kt
@@ -2,6 +2,7 @@ package eu.alkismavridis.euljava.parser.token
 
 import eu.alkismavridis.euljava.core.CompileOptions
 import eu.alkismavridis.euljava.core.EulLogger
+import eu.alkismavridis.euljava.test_utils.EulAssert.Companion.assertEulReference
 import eu.alkismavridis.euljava.test_utils.EulAssert.Companion.assertOperator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -13,17 +14,32 @@ internal class SpecialCharacterTokenizerTest {
     private val options = CompileOptions("")
 
     @Test
-    fun shouldEndWithOperator() {
-        val tokenizer = this.createTokenizer("==")
+    fun shouldProperlyReadEqualsOperators() {
+        val tokenizer = this.createTokenizer("== == = ===")
         assertOperator(tokenizer.getNextToken(true), "==", 1, 1);
+        assertOperator(tokenizer.getNextToken(true), "==", 1, 4);
+        assertOperator(tokenizer.getNextToken(true), "=", 1, 7);
+        assertOperator(tokenizer.getNextToken(true), "===", 1, 9);
         assertThat(tokenizer.getNextToken(true)).isNull();
     }
 
     @Test
-    fun shouldReadSeperateOperators() {
-        val tokenizer = this.createTokenizer("== =")
-        assertOperator(tokenizer.getNextToken(true), "==", 1, 1);
-        assertOperator(tokenizer.getNextToken(true), "=", 1, 4);
+    fun shouldProperlyReadPlusOperators() {
+        val tokenizer = this.createTokenizer("++ += + ")
+        assertOperator(tokenizer.getNextToken(true), "++", 1, 1);
+        assertOperator(tokenizer.getNextToken(true), "+=", 1, 4);
+        assertOperator(tokenizer.getNextToken(true), "+", 1, 7);
+        assertThat(tokenizer.getNextToken(true)).isNull();
+    }
+
+    @Test
+    fun shouldProperlyReadOperatorsWithStrings() {
+        val tokenizer = this.createTokenizer("aa++aa-- +")
+        assertEulReference(tokenizer.getNextToken(true), "aa", 1, 1);
+        assertOperator(tokenizer.getNextToken(true), "++", 1, 3);
+        assertEulReference(tokenizer.getNextToken(true), "aa", 1, 5);
+        assertOperator(tokenizer.getNextToken(true), "--", 1, 7);
+        assertOperator(tokenizer.getNextToken(true), "+", 1, 10);
         assertThat(tokenizer.getNextToken(true)).isNull();
     }
 
